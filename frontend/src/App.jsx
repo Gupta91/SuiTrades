4394bbe2a828a3e57779342457182ffbeca5eca2
veermshah <veermickey@gmail.com>
2024-05-29 09:57:14 -0700

    added frontend, routing, home page

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+
+function App() {
+    const [count, setCount] = useState(0);
+
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
+}
+
+export default App;
